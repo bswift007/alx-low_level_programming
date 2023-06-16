b261fe5eb99b742a620a5a7c0a95ab3658097039
bswift007 <cuteadex@yahoo.com>
2023-06-15 22:37:11 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A C program that prints exactly a line using printf function
+ * Return: 0 (Passed)
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
