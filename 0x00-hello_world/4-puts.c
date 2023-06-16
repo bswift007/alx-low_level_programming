7c1800c6289170426fd5dd8fc5e3d9f9d2cbe6e8
bswift007 <cuteadex@yahoo.com>
2023-06-15 22:28:22 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - The program for printing a line with puts function
+ * Return: 0 (Passed)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
